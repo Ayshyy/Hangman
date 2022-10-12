d7e48a9cfe608ba1cd41623a28543f2018c5484d
valentin loisel <valentin-loisel33@outlook.fr>
2022-10-12 14:10:12 +0200

    select Word

--- Hangman/main.go
+++ Hangman/main.go
@@ -4,27 +4,45 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 var (
-	isStart bool
-	words   string
+	isStart     bool
+	words       string
+	hangmanData HangManData
 )
+
+type HangManData struct {
+	Word             string     // Word composed of '_', ex: H_ll_
+	ToFind           string     // Final word chosen by the program at the beginning. It is the word to find
+	Attempts         int        // Number of attempts left
+	HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
+}
 
 func main() {
 	load()
-	Init()
+	//Init()
+	Debug() // temp fonction delete after finish
+}
+
+func Debug() {
+	fmt.Println(hangmanData.ToFind)
 }
 
 func load() {
 	//Load File words.txt in memory
+	fmt.Println("Hangman is Loading...")
 	content, err := ioutil.ReadFile("words.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	words = string(content)
-
+	//Init hangman Data
+	//Select random word
+	hangmanData.ToFind = selectword(getRandomNumber(WordsSize(), 0))
 }
 
 func selectword(line int) string {
@@ -34,6 +52,20 @@ func selectword(line int) string {
 		}
 	}
 	return ""
+}
+
+func getRandomNumber(max int, min int) int {
+	rand.Seed(time.Now().UnixNano())
+	result := rand.Intn(max-min) + min
+	return result
+}
+
+func WordsSize() int {
+	result := 0
+	for range strings.Split(words, "\n") {
+		result++
+	}
+	return result
 }
 
 func Init() {
