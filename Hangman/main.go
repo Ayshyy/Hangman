b6dd681db3176d8332f533a8e99e70ff199b2873
valentin loisel <valentin-loisel33@outlook.fr>
2022-10-12 10:31:23 +0200

    Init operation

--- Hangman/main.go
+++ Hangman/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+var (
+	isStart bool = false
+)
+
+func main() {
+	Init()
+}
+
+func Init() {
+	input := ""
+	fmt.Println("Appuyez sur une touche pour commencé !")
+	fmt.Scanf(">", &input)
+	for len(input) > 1 {
+	}
+	isStart = true
 }
