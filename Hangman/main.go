4b0619968049be6bff116534c967407eb440bc37
valentin loisel <valentin-loisel33@outlook.fr>
2022-10-12 15:32:35 +0200

    Hangman position

--- Hangman/main.go
+++ Hangman/main.go
@@ -5,22 +5,22 @@ import (
 	"io/ioutil"
 	"log"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 )
 
 var (
-	isStart          bool
-	words            string
-	HangmanPositions string
-	hangmanData      HangManData
+	isStart                 bool   //Check if party is Running
+	words, HangmanPositions string //Data from file hangman.txt and word.txt
+	hangmanData             HangManData
 )
 
 type HangManData struct {
-	Word             string     // Word composed of '_', ex: H_ll_
-	ToFind           string     // Final word chosen by the program at the beginning. It is the word to find
-	Attempts         int        // Number of attempts left
-	HangmanPositions [10]string // It can be the array where the positions parsed in "hangman.txt" are stored
+	Word             string // Word composed of '_', ex: H_ll_
+	ToFind           string // Final word chosen by the program at the beginning. It is the word to find
+	Attempts         int    // Number of attempts left
+	HangmanPositions int    // It can be the array where the positions parsed in "hangman.txt" are stored
 }
 
 func main() {
@@ -31,6 +31,11 @@ func main() {
 
 func Debug() {
 	fmt.Println(hangmanData.ToFind)
+	fmt.Println("---------------")
+	for i := 0; i <= 10; i++ {
+		fmt.Println(getHangmanFromPos(i))
+	}
+	fmt.Println("---------------")
 }
 
 func load() {
@@ -44,7 +49,9 @@ func load() {
 	words = string(content)
 
 	//Select random word
-	hangmanData.ToFind = selectword(getRandomNumber(WordsSize(), 0))
+	hangmanData.ToFind = SelectWord(getRandomNumber(WordsSize(), 0))
+
+	//Load Hangman.txt
 	hangman, err := ioutil.ReadFile("hangman.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -52,7 +59,7 @@ func load() {
 	HangmanPositions = string(hangman)
 }
 
-func selectword(line int) string {
+func SelectWord(line int) string {
 	for u, e := range strings.Split(words, "\n") {
 		if u == line-1 {
 			return e
@@ -73,6 +80,23 @@ func getRandomNumber(max, min int) int {
 	rand.Seed(time.Now().UnixNano())
 	result := rand.Intn(max-min) + min
 	return result
+}
+
+func getHangmanFromPos(position int) string {
+	HangmanSize := 8
+	if position > 10 && position < 1 {
+		fmt.Printf("Hangman position Error: %v", position)
+		os.Exit(3)
+		return ""
+	}
+	hangmanStat := ""
+	for index, r := range strings.Split(HangmanPositions, "\n") {
+		INC := HangmanSize * position
+		if index >= INC-HangmanSize-1 && index < INC-1 {
+			hangmanStat += r + "\n"
+		}
+	}
+	return hangmanStat
 }
 
 func Init() {
